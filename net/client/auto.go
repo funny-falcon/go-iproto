f5ecabb50c970055301263c6090847eb69a45395
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2013-09-11 17:53:51 +0400

    net/client primitive autoconnector

---
+++ net/client/auto.go
@@ -0,0 +1,50 @@
+package client
+
+import (
+	"github.com/funny-falcon/go-iproto"
+	"sync"
+)
+
+type Auto struct {
+	Config *ServerConfig
+	servers map[string]*Server
+	sync.Mutex
+}
+
+func (a *Auto) Get(addr string) (serv *Server) {
+	if a.servers == nil {
+		a.Lock()
+		if a.servers == nil {
+			a.servers = make(map[string]*Server, len(a.servers))
+		}
+		a.Unlock()
+	}
+	if serv = a.servers[addr]; serv == nil {
+		a.Lock()
+		if serv = a.servers[addr]; serv == nil {
+			conf := *a.Config
+			conf.Address = addr
+			serv = conf.NewServer()
+			iproto.Run(serv)
+			newServs := make(map[string]*Server, len(a.servers))
+			newServs[addr] = serv
+			for a, s := range a.servers {
+				newServs[a] = s
+			}
+			a.servers = newServs
+		}
+		a.Unlock()
+	}
+	return
+}
+
+func (a *Auto) Stop() {
+	a.Lock()
+	if a.servers != nil {
+		for _, serv := range a.servers {
+			serv.Stop()
+		}
+		a.servers = nil
+	}
+	a.Unlock()
+}
